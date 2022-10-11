fd427fe7af94839fc76caaa5439eaf3491a316ef
rfasolino <84328521+rfasolino@users.noreply.github.com>
2022-10-11 14:37:58 -0300

    Add files via upload

---
+++ Ejercicio 2/TP 6 Ejercicios/EJ2.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EJ2
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.txt3 = New System.Windows.Forms.TextBox()
+        Me.BtnI = New System.Windows.Forms.Button()
+        Me.BtnS = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Mayor 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Mayor 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Mayor 3"
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(95, 63)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(100, 20)
+        Me.txt1.TabIndex = 5
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(95, 89)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(100, 20)
+        Me.txt2.TabIndex = 6
+        '
+        'txt3
+        '
+        Me.txt3.Location = New System.Drawing.Point(95, 115)
+        Me.txt3.Name = "txt3"
+        Me.txt3.Size = New System.Drawing.Size(100, 20)
+        Me.txt3.TabIndex = 7
+        '
+        'BtnI
+        '
+        Me.BtnI.Location = New System.Drawing.Point(25, 152)
+        Me.BtnI.Name = "BtnI"
+        Me.BtnI.Size = New System.Drawing.Size(84, 43)
+        Me.BtnI.TabIndex = 8
+        Me.BtnI.Text = "Ingresar"
+        Me.BtnI.UseVisualStyleBackColor = True
+        '
+        'BtnS
+        '
+        Me.BtnS.Location = New System.Drawing.Point(115, 152)
+        Me.BtnS.Name = "BtnS"
+        Me.BtnS.Size = New System.Drawing.Size(80, 43)
+        Me.BtnS.TabIndex = 9
+        Me.BtnS.Text = "Salir"
+        Me.BtnS.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.BtnS)
+        Me.Controls.Add(Me.BtnI)
+        Me.Controls.Add(Me.txt3)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txt1 As System.Windows.Forms.TextBox
+    Friend WithEvents txt2 As System.Windows.Forms.TextBox
+    Friend WithEvents txt3 As System.Windows.Forms.TextBox
+    Friend WithEvents BtnI As System.Windows.Forms.Button
+    Friend WithEvents BtnS As System.Windows.Forms.Button
+
+End Class
---
+++ Ejercicio 2/TP 6 Ejercicios/EJ2.vb
@@ -0,0 +1,55 @@
+﻿Public Class EJ2
+
+    Private Sub BtnI_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnI.Click
+        Dim vector(10) As Integer
+        Dim F As Integer
+        Dim Prim As Integer = 0
+        Dim Seg As Integer = 0
+        Dim Ter As Integer = 0
+
+        For F = 1 To 10
+
+            vector(F) = InputBox("Ingresar numero " & F)
+
+        Next
+
+        For F = 1 To 10
+
+            If vector(F) > Prim Then
+
+                Prim = vector(F)
+
+            End If
+
+        Next
+
+        txt1.Text = Prim
+
+        For F = 1 To 10
+
+            If vector(F) > Seg And vector(F) <> Prim Then
+
+                Seg = vector(F)
+
+            End If
+
+        Next
+
+        txt2.Text = Seg
+
+        For F = 1 To 10
+
+            If vector(F) > Ter And vector(F) <> Prim And vector(F) <> Seg Then
+
+                Ter = vector(F)
+
+            End If
+
+        Next
+        txt3.Text = Ter
+    End Sub
+
+    Private Sub BtnS_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnS.Click
+        End
+    End Sub
+End Class
